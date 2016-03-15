ddd535b48ab02e08803c19233dd9d2b1863d284f
reformanda <working1008@gmail.com>
2016-03-14 22:53:41 -0400

    update warbler.rb

--- config/warble.rb
+++ config/warble.rb
@@ -1,3 +1,10 @@
 Warbler::Config.new do |config|
   config.webxml.rails.env = 'development'
+
+  config.dirs = %w(app db config lib log vendor tmp bin)
+
+  config.includes = FileList["Rakefile"]
+
+  config.bundle_without = []
+
 end
