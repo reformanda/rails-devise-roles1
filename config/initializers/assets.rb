e7bdee29fc1334ad5f683a402b93dae50e382ce6
reformanda <working1008@gmail.com>
2016-04-28 23:52:25 -0400

    stub assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( nomination_types.css nomination_types.js awards.js awards.css uielements.css uielements.js widgets.css widgets.js forms.css forms.js pages.css pages.js search.js dashboards.css dashboards.js )
+Rails.application.config.assets.precompile += %w( nomination_types.css awards.js awards.css uielements.css uielements.js widgets.css widgets.js forms.css forms.js pages.css pages.js search.js dashboards.css dashboards.js )
