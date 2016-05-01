09172380e8b8bb634a4b39470806520a6ea328f4
reformanda <working1008@gmail.com>
2016-04-30 22:59:49 -0400

    change datatype

--- db/migrate/20160425235538_create_nomination_types.rb
+++ db/migrate/20160425235538_create_nomination_types.rb
@@ -3,7 +3,7 @@ class CreateNominationTypes < ActiveRecord::Migration
     create_table :nomination_types do |t|
       t.string :year
       t.string :code
-      t.string :description
+      t.text :description
       t.string :awards_announcement
       t.string :implementing_instructions
       t.string :submission_form
