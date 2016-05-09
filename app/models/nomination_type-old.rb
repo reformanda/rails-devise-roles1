32a08dda5129f02ba3e38fed90409af5d40c27a9
reformanda <working1008@gmail.com>
2016-05-08 21:47:32 -0400

    cleanup

--- app/models/nomination_type-old.rb
+++
@@ -1,9 +0,0 @@
-class NominationType < ActiveRecord::Base
-  #has_many :nominations
-  mount_uploader :awards_announcement, AwardsAnnouncementUploader
-  mount_uploader :implementing_instructions, ImplementingInstructionsUploader
-  mount_uploader :submission_form_and_award_narrative, SubmissionFormUploader
-  mount_uploader :award_narrative_sample, AwardNarrativeUploader
-  mount_uploader :guidelines_and_tips, GuidelinesUploader
-
-end
--- app/views/nomination_types-old/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @nomination_type, :id, :created_at, :updated_at
