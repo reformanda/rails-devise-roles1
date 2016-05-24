5247babe51142301caab0744afefe582cae008cc
reformanda <working1008@gmail.com>
2016-05-24 07:59:43 -0400

    force using fog

--- config/initializers/S3.rb
+++ config/initializers/S3.rb
@@ -1,19 +1,12 @@
 CarrierWave.configure do |config|
-
-
-  if Rails.env.development? || Rails.env.test?
-      config.storage = :file
-  else   
-      config.storage = :fog
-      config.fog_credentials = {
+  config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['S3_KEY'],
       :aws_secret_access_key  => ENV['S3_SECRET']
-
       # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
-    }
-    config.fog_directory  = ENV['S3_BUCKET']
+  }
+  config.fog_directory  = ENV['S3_BUCKET']
 
-    config.fog_use_ssl_for_aws = true
-  end
+  config.fog_use_ssl_for_aws = true
+
 end
