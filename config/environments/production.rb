11f5ad9f9c272002742a826f58526ee2f11f8583
reformanda <working1008@gmail.com>
2017-06-03 10:50:52 -0400

    no ssl verification for production smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
 #    domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
+    openssl_verify_mode: "none",
 #    user_name: Rails.application.secrets.email_provider_username,
 #    password: Rails.application.secrets.email_provider_password
   }
